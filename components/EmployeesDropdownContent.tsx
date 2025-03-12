1cdf93281fe9d73545d03cdd9410be74963d2274
nikachitava <chitava18.official@gmail.com>
2025-03-12 23:00:25 +0400

    working on dropdown contents

--- components/EmployeesDropdownContent.tsx
+++ components/EmployeesDropdownContent.tsx
@@ -1,18 +1,50 @@
 import { IEmployeesReqResponse } from "@/types/IEmployeesReqResponse";
-import React from "react";
+import React, { useState } from "react";
+import CustomButton from "./CustomButton";
+import DropdownCheckboxItem from "./DropdownCheckboxItem";
 
 const EmployeesDropdownContent = ({
     employees,
 }: {
     employees: IEmployeesReqResponse[];
 }) => {
+    const [selectedEmployees, setSelectedEmployees] = useState<number[]>([]);
+
+    const handleSelect = (id: number) => {
+        setSelectedEmployees((prevSelected) =>
+            prevSelected.includes(id)
+                ? prevSelected.filter((implyId) => implyId !== id)
+                : [...prevSelected, id]
+        );
+    };
+
+    const handleContentClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
+    };
+
     return (
-        <div className="absolute left-0 right-0 top-full z-20 mt-[11px] w-[688px] border-[0.5px] border-[#8338EC] rounded-[10px] px-[30px] py-10">
+        <div
+            className="absolute left-0 right-0 top-full z-20 mt-[11px] w-[688px] border-[0.5px] border-[#8338EC] rounded-[10px] px-[30px] py-10 space-y-[22px] bg-white"
+            onClick={handleContentClick}
+        >
             {employees.map((employee) => (
-                <div key={employee.id}>
-                    <p>{employee.name}</p>
-                </div>
+                <DropdownCheckboxItem
+                    key={employee.id}
+                    title={employee.name + " " + employee.surname}
+                    isSelected={selectedEmployees.includes(employee.id)}
+                    onSelect={() => handleSelect(employee.id)}
+                    icon={employee.avatar}
+                />
             ))}
+
+            <div className="flex justify-end">
+                <CustomButton
+                    title="არჩევა"
+                    onClick={() => {}}
+                    otherStyles="bg-purple-accent text-white rounded-[20px] !px-[20px] !py-2"
+                    fill={true}
+                />
+            </div>
         </div>
     );
 };
--- components/PriorityDropdownContent.tsx
+++ components/PriorityDropdownContent.tsx
@@ -1,18 +1,49 @@
 import { IPrioritiesReqResponse } from "@/types/IPrioritiesReqResponse";
-import React from "react";
+import React, { useState } from "react";
+import DropdownCheckboxItem from "./DropdownCheckboxItem";
+import CustomButton from "./CustomButton";
 
 const PriorityDropdownContent = ({
     priorities,
 }: {
     priorities: IPrioritiesReqResponse[];
 }) => {
+    const [selectedPriorities, setSelectedPriorities] = useState<number[]>([]);
+
+    const handleSelect = (id: number) => {
+        setSelectedPriorities((prevSelected) =>
+            prevSelected.includes(id)
+                ? prevSelected.filter((priorityId) => priorityId !== id)
+                : [...prevSelected, id]
+        );
+    };
+
+    const handleContentClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
+    };
+
     return (
-        <div className="absolute left-0 right-0 top-full z-20 mt-[11px] w-[688px] border-[0.5px] border-[#8338EC] rounded-[10px] px-[30px] py-10">
+        <div
+            className="absolute left-0 right-0 top-full z-20 mt-[11px] w-[688px] border-[0.5px] border-[#8338EC] rounded-[10px] px-[30px] py-10 space-y-[22px] bg-white"
+            onClick={handleContentClick}
+        >
             {priorities.map((priority) => (
-                <div key={priority.id}>
-                    <p>{priority.name}</p>
-                </div>
+                <DropdownCheckboxItem
+                    key={priority.id}
+                    title={priority.name}
+                    isSelected={selectedPriorities.includes(priority.id)}
+                    onSelect={() => handleSelect(priority.id)}
+                />
             ))}
+
+            <div className="flex justify-end">
+                <CustomButton
+                    title="არჩევა"
+                    onClick={() => {}}
+                    otherStyles="bg-purple-accent text-white rounded-[20px] !px-[20px] !py-2"
+                    fill={true}
+                />
+            </div>
         </div>
     );
 };
