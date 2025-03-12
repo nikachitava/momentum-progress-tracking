c41c9cd7a9040189fc2ac48207374331e056842b
nikachitava <chitava18.official@gmail.com>
2025-03-12 16:14:35 +0400

    prefetch data to use in client components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,45 @@
-export default function Home() {
+import FilterMenu from "@/components/FilterMenu";
+import { IDepartmentReqResponse } from "@/types/IDepartmentReqResponse";
+import { IEmployeesReqResponse } from "@/types/IEmployeesReqResponse";
+import { IPrioritiesReqResponse } from "@/types/IPrioritiesReqResponse";
+import React from "react";
+
+const Home = async () => {
+    const departmentsRes = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/departments`
+    );
+    const departments: IDepartmentReqResponse[] = await departmentsRes.json();
+
+    const prioritiesRes = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/priorities`
+    );
+    const priorities: IPrioritiesReqResponse[] = await prioritiesRes.json();
+
+    const employeesRes = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/employees`,
+        {
+            headers: {
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_BEARER_TOKEN}`,
+                "Content-Type": "application/json",
+            },
+        }
+    );
+    const employees: IEmployeesReqResponse[] = await employeesRes.json();
+
     return (
-        <main className="container">
-            <h1 className="text-orange font-medium">დავალების გვერდი</h1>
+        <main>
+            <div className="container mt-10">
+                <h1 className="font-semibold text-[34px] text-grey-shades-headlines">
+                    დავალების გვერდი
+                </h1>
+                <FilterMenu
+                    departments={departments}
+                    priorities={priorities}
+                    employees={employees}
+                />
+            </div>
         </main>
     );
-}
+};
+
+export default Home;
