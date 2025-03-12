c51f3f28d360fb1a08d2042341554ee88d374fe4
nikachitava <chitava18.official@gmail.com>
2025-03-12 16:14:11 +0400

    use next Image instead of html img tag

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,13 +1,13 @@
-import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const NavBar = () => {
     return (
         <header>
             <div className="container flex items-center justify-between py-[31px]">
                 <Link href={"/"}>
-                    {" "}
-                    <img src="/logo.svg" />
+                    <Image alt="logo" src="/logo.svg" width={210} height={38} />
                 </Link>
                 <button>SUBMIT</button>
             </div>
