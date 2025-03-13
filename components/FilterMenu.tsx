d51d4609ec41f9ee52ec7ca04ab7619e7131512e
nikachitava <chitava18.official@gmail.com>
2025-03-13 17:57:34 +0400

    add FilterMenuSelectedOptions in layout

--- components/FilterMenu.tsx
+++ components/FilterMenu.tsx
@@ -6,6 +6,7 @@ import DepartmentDropdownContent from "./DepartmentDropdownContent";
 import { IFilterMenu } from "@/types/IFilterMenu";
 import PriorityDropdownContent from "./PriorityDropdownContent";
 import EmployeesDropdownContent from "./EmployeesDropdownContent";
+import FilterMenuSelectedOptions from "./FilterMenuSelectedOptionsContainer";
 
 const FilterMenu: React.FC<IFilterMenu> = ({
     departments,
@@ -19,28 +20,35 @@ const FilterMenu: React.FC<IFilterMenu> = ({
     };
 
     return (
-        <div className="relative max-w-[688px] flex items-center justify-between border border-[#DEE2E6] rounded-[10px] mt-[52px]">
-            <FilterMenuItem
-                title="დეპარტამენტი"
-                isOpen={openMenu === "დეპარტამენტი"}
-                onClick={() => handleMenuClick("დეპარტამენტი")}
-                children={
-                    <DepartmentDropdownContent departments={departments} />
-                }
-            />
-            <FilterMenuItem
-                title="პრიორიტეტი"
-                isOpen={openMenu === "პრიორიტეტი"}
-                onClick={() => handleMenuClick("პრიორიტეტი")}
-                children={<PriorityDropdownContent priorities={priorities} />}
-            />
-            <FilterMenuItem
-                title="თანამშრომელი"
-                isOpen={openMenu === "თანამშრომელი"}
-                onClick={() => handleMenuClick("თანამშრომელი")}
-                children={<EmployeesDropdownContent employees={employees} />}
-            />
-        </div>
+        <>
+            <div className="relative max-w-[688px] flex items-center justify-between border border-[#DEE2E6] rounded-[10px] mt-[52px]">
+                <FilterMenuItem
+                    title="დეპარტამენტი"
+                    isOpen={openMenu === "დეპარტამენტი"}
+                    onClick={() => handleMenuClick("დეპარტამენტი")}
+                    children={
+                        <DepartmentDropdownContent departments={departments} />
+                    }
+                />
+                <FilterMenuItem
+                    title="პრიორიტეტი"
+                    isOpen={openMenu === "პრიორიტეტი"}
+                    onClick={() => handleMenuClick("პრიორიტეტი")}
+                    children={
+                        <PriorityDropdownContent priorities={priorities} />
+                    }
+                />
+                <FilterMenuItem
+                    title="თანამშრომელი"
+                    isOpen={openMenu === "თანამშრომელი"}
+                    onClick={() => handleMenuClick("თანამშრომელი")}
+                    children={
+                        <EmployeesDropdownContent employees={employees} />
+                    }
+                />
+            </div>
+            <FilterMenuSelectedOptions />
+        </>
     );
 };
 
