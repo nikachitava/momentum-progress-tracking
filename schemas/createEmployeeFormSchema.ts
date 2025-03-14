ea407af8fdd1bb07cc34a57903a81558a5bf9151
nikachitava <chitava18.official@gmail.com>
2025-03-14 23:39:54 +0400

    fix avatar validation schema

--- schemas/createEmployeeFormSchema.ts
+++ schemas/createEmployeeFormSchema.ts
@@ -28,29 +28,28 @@ export const createEmployeeFormSchema = z.object({
             message: "გვარი უნდა შეიცავდეს მხოლოდ ქართულ ან ლათინურ სიმბოლოებს",
         }),
     avatar: z
-        .custom<File>((file) => file instanceof File, {
-            message: "ატვირთული ფაილი არ არის ვალიდური",
+        .any()
+        .refine((files) => files?.length > 0, {
+            message: "სურათი სავალდებულოა",
         })
-        .refine((file) => file.size <= MAX_FILE_SIZE, {
-            message: "ფაილის ზომა არ უნდა აღემატებოდეს 600კბ-ს",
+        .refine((files) => files?.[0]?.size <= MAX_FILE_SIZE, {
+            message: `ფაილის ზომა უნდა იყოს მაქსიმუმ 600kb`,
         })
-        .refine((file) => ACCEPTED_IMAGE_TYPES.includes(file.type), {
-            message: `დასაშვებია მხოლოდ შემდეგი ფორმატები: ${ACCEPTED_IMAGE_TYPES.join(
-                ", "
-            )}`,
+        .refine((files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type), {
+            message: `გთხოვთ ატვირთოთ მხოლოდ სურათის ფაილები (JPEG, JPG, PNG)`,
         }),
     departmentId: z.string({
         required_error: "გთხოვთ აირჩიოთ დეპარტამენტი",
     }),
 });
 
-// export type CreateEmployeeFormSchemaType = z.infer<
-//     typeof createEmployeeFormSchema
-// >;
+export type CreateEmployeeFormSchemaType = z.infer<
+    typeof createEmployeeFormSchema
+>;
 
-export type CreateEmployeeFormSchemaType = {
-    firstName: string;
-    lastName: string;
-    avatar: File;
-    departmentId: string;
-}
+// export type CreateEmployeeFormSchemaType = {
+//     firstName: string;
+//     lastName: string;
+//     avatar: File;
+//     departmentId: string;
+// }
