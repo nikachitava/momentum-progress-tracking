3ac4d76cb927294297d95674e50ab78a78570559
nikachitava <chitava18.official@gmail.com>
2025-03-17 21:32:37 +0400

    create route for single task

---
+++ app/task/[id]/page.tsx
@@ -0,0 +1,29 @@
+import CommentsSection from "@/components/CommentsSection";
+import TaskDetailsContainer from "@/components/TaskDetailsContainer";
+import { getComments } from "@/services/data/comments";
+import { getStatuses } from "@/services/data/statuses";
+import { getSingleTask } from "@/services/data/tasks";
+import React from "react";
+
+const SingleTaskPage = async ({
+    params,
+}: {
+    params: Promise<{ id: number }>;
+}) => {
+    const id = (await params).id;
+
+    const task = await getSingleTask(id);
+    const comments = await getComments(id);
+    const statuses = await getStatuses();
+
+    return (
+        <div>
+            <div className="container mt-10 pb-20 flex justify-between">
+                <TaskDetailsContainer task={task} statuses={statuses} />
+                <CommentsSection comments={comments} />
+            </div>
+        </div>
+    );
+};
+
+export default SingleTaskPage;
