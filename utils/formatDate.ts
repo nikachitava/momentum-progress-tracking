124fb9d1685da2b3f4bb9eb7c16cc8fedda4c9c1
nikachitava <chitava18.official@gmail.com>
2025-03-18 21:27:03 +0400

    change formatDate method to avoid hydration issue

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,8 +1,14 @@
 export const formatDate = (isoDate: string): string => {
     const date = new Date(isoDate);
-    return date.toLocaleDateString("en-US", {
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-    });
-};
+    
+    const months = [
+      'იანვარი', 'თებერველი', 'მარტი', 'აპრილი', 'მაისი', 'ივნისი',
+      'ივლისი', 'აგვისტო', 'სექტემბერი', 'ოქტომბერი', 'ნოემბერი', 'დეკემბერი'
+    ];
+    
+    const day = date.getUTCDate();
+    const month = months[date.getUTCMonth()];
+    const year = date.getUTCFullYear();
+    
+    return `${month} ${day}, ${year}`;
+  }
